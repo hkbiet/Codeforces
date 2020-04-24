3ceef2d78dbcbe2eca579c2ac27d44f273a446d4
Hemant Kumar <hkbiet@gmail.com>
2020-04-24 10:00:16 +0530

    optimal in O(1)

---
+++ watermelon_optimal.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+
+
+	int weight;
+	scanf("%d",&weight);
+
+	if(weight % 2 == 0 && weight > 2){
+	
+		printf("YES");
+	
+	}else{
+
+		printf("NO");
+
+	}
+
+	return 0;
+}
